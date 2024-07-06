66e2ad2cb5cd5ddf59bcb6f6654b914a550ba594
derossm <derossm@gmail.com>
2024-07-06 10:53:39 -0700

    Update mteFunctions.pas
    fixed to work with current xedit

--- Edit Scripts/mteFunctions.pas
+++ Edit Scripts/mteFunctions.pas
@@ -146,16 +146,16 @@ unit mteFunctions;
 
 implementation
 
-{
 const
+{
   bethesdaFiles = 'Skyrim.esm'#13'Update.esm'#13'Dawnguard.esm'#13'HearthFires.esm'#13
   'Dragonborn.esm'#13'Fallout3.esm'#13'FalloutNV.esm'#13'Oblivion.esm'#13
   'Skyrim.Hardcoded.keep.this.with.the.exe.and.otherwise.ignore.it.I.really.mean.it.dat'#13
   'Fallout3.Hardcoded.keep.this.with.the.exe.and.otherwise.ignore.it.I.really.mean.it.dat'#13
   'Oblivion.Hardcoded.keep.this.with.the.exe.and.otherwise.ignore.it.I.really.mean.it.dat'#13
   'FalloutNV.Hardcoded.keep.this.with.the.exe.and.otherwise.ignore.it.I.really.mean.it.dat';
-  GamePath = DataPath + '..\';
 }
+  GamePath = DataPath + '..\';
 
 type
   TColor = Record
